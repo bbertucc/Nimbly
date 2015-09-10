a6049cf9020d66362968c447e2d391beacea7e47
bbertucc <b@Bs-Mac-mini.local>
2015-09-10 11:20:41 -0500

    Featured Articles Thumbnail and Style Updates
    Resolved thumbnail image fallback issue, added each post to grid,
    updated grid to scale three span and lower so that nested images
    maintain clarity.

--- nimbly-theme/parts/featured_articles.php
+++ nimbly-theme/parts/featured_articles.php
@@ -10,15 +10,11 @@
         if($featured_articles_title) echo '<h1 class="section_title">'.$featured_articles_title.'</h2>';
       ?>
       <?php  while ( $the_query->have_posts() ) : $the_query->the_post();?>
-      <a title="Link to <?php the_title();?>" href="<?php the_permalink();?>">
+      <a title="Link to <?php the_title();?>" href="<?php the_permalink();?>" class="three_span">
         <?php 
-          $hero_media_type = get_field('hero_media_type');
-          $hero_image = get_field('hero_image'); 
-          $hero_image_fallback = get_field('hero_image_fallback'); 
-          if($hero_media_type == 'image') $thumbnail_hero_url = $hero_image['sizes'][thumbnail]; 
-          if($hero_media_type == 'video_loop') $thumbnail_hero_url = $hero_image_fallback['sizes'][thumbnail];
-          echo '<img alt="Image for '.get_the_title().'" src="'.$thumbnail_hero_url.'">';
+          include 'hero_image_variables.php';
         ?><?php echo $post_id ?>
+        <span class="thumbnail" style="background-image: url(<?php echo $thumbnail_hero_url; ?>)"></span>
         <span class="category"><?php echo get_the_category()[0]->cat_name;?></span>
         <span class="title"><?php the_title();?></span>
       </a>
