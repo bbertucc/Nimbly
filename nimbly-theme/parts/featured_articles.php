b23824dc6f5c1a284970fdeec736dd9fff4993c8
Blake Bertuccelli <blakebertuccelli@gmail.com>
2015-09-20 21:52:21 -0500

    wp_query now limits from category set in customizer

--- nimbly-theme/parts/featured_articles.php
+++ nimbly-theme/parts/featured_articles.php
@@ -1,19 +1,24 @@
 <?php 
-  $args = array( 'posts_per_page' => 8, 'post__in' => get_option('sticky_posts'), 'ignore_sticky_posts' => true);
+  $featured_articles_category = get_theme_mod( 'featured_articles_category'); 
+  $args = array( 'posts_per_page' => -1, 'cat' => $featured_articles_category);
   $the_query = new WP_Query($args);
   if ($the_query->have_posts()):
   ?>
   <div id="featured_articles" style="background-image:url(<?php echo get_theme_mod( 'custom_background_image' ); ?>)">
     <div class="container row">
-      <?php 
-        $featured_articles_title = get_theme_mod( 'featured_articles_title'); 
-        if($featured_articles_title) echo '<h1 class="section_title">'.$featured_articles_title.'</h2>';
-      ?>
+      <h1 class="section_title">
+        <?php if($featured_articles_category){
+          $featured_articles_category_name = get_cat_name($featured_articles_category);
+          $featured_articles_category_link = get_category_link($featured_articles_category);
+          echo '<a href="'.$featured_articles_category_link.'" title="Link to '.$featured_articles_category_name.' Category Page">'.$featured_articles_category_name.'</a>';
+          }else{
+            echo 'Featured Articles';
+          }
+        ?>
+      </h1>
       <?php  while ( $the_query->have_posts() ) : $the_query->the_post();?>
-      <a title="Link to <?php the_title();?>" href="<?php the_permalink();?>" class="three_span">
-        <?php 
-          include 'hero_image_variables.php';
-        ?><?php echo $post_id ?>
+      <a title="Link to <?php the_title();?>" href="<?php the_permalink();?>" class="three_span post">
+        <?php include 'hero_image_variables.php'; ?>
         <span class="thumbnail" style="background-image: url(<?php echo $thumbnail_hero_url; ?>)"></span>
         <span class="category"><?php echo get_the_category()[0]->cat_name;?></span>
         <span class="title"><?php the_title();?></span>
