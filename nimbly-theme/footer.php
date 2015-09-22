5da76363f211e629aff98b1b35afec9ecd35eab8
Blake Bertuccelli <blakebertuccelli@gmail.com>
2015-09-21 20:11:26 -0500

    Made footer completed widget-ized

--- nimbly-theme/footer.php
+++ nimbly-theme/footer.php
@@ -1,22 +1,6 @@
   <div id="footer">
     <div class="container row">
-      <div class="four_span">
-        <a title="<?php bloginfo( 'name' ); ?> - Home" href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod( 'inverted_logo'); ?>" class="inverted_logo" alt="<?php bloginfo( 'name' ); ?> - Inverted Logo"></a>
-        <h3><?php echo get_theme_mod( 'contact_tagline'); ?></h3>
-        <div class="contact_links">
-          <a title="Email Link" href="mailto:<?php echo get_theme_mod( 'contact_email'); ?>" title="Email"><?php echo get_theme_mod( 'contact_email'); ?></a>
-				  <a target="blank" title="Twitter Link" href="<?php echo get_theme_mod( 'twitter_url'); ?>"><i class="fa fa-twitter fa-lg"></i></a> 
-          <a target="blank" title="Facebook Link" href="<?php echo get_theme_mod( 'fb_url'); ?>"><i class="fa fa-facebook fa-lg"></i></a> 
-          <a target="blank" title="LinkedIn Link" href="<?php echo get_theme_mod( 'linkedin_url'); ?>"><i class="fa fa-linkedin fa-lg"></i></a> 
-        </div>
-      </div>
-      <div class="eight_span">
-        <?php 
-          dynamic_sidebar( 'footer_widget_one' );
-          dynamic_sidebar( 'footer_widget_two' ); 
-          dynamic_sidebar( 'footer_widget_three' ); 
-        ?>
-      </div>
+      <?php dynamic_sidebar( 'footer_widgets' ); ?>
     </div>
     <p><?php echo get_theme_mod( 'copyright_information'); ?></p>
   </div>
--- nimbly-theme/includes/theme_widgets.php
+++ nimbly-theme/includes/theme_widgets.php
@@ -1,25 +1,9 @@
 <?php
 function main_footer_widget_init() {
 	register_sidebar( array(
-		'name'          => 'Footer Widget One',
-		'id'            => 'footer_widget_one',
-		'before_widget' => '<div class="four_span">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3>',
-		'after_title'   => '</h3>',
-	) );
-	register_sidebar( array(
-		'name'          => 'Footer Widget Two',
-		'id'            => 'footer_widget_two',
-		'before_widget' => '<div class="four_span">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3>',
-		'after_title'   => '</h3>',
-	) );
-	register_sidebar( array(
-		'name'          => 'Footer Widget Three',
-		'id'            => 'footer_widget_three',
-		'before_widget' => '<div class="four_span">',
+		'name'          => 'Footer Widgets',
+		'id'            => 'footer_widgets',
+		'before_widget' => '<div class="three_span widget">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h3>',
 		'after_title'   => '</h3>',
