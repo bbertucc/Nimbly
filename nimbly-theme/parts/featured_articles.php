fb9a1e98538d7cd914c1b09ab9781bafc465a98d
Blake Bertuccelli <blakebertuccelli@gmail.com>
2015-09-21 20:15:32 -0500

    featured_article section now Category-specific

--- nimbly-theme/parts/featured_articles.php
+++ nimbly-theme/parts/featured_articles.php
@@ -1,6 +1,6 @@
 <?php 
   $featured_articles_category = get_theme_mod( 'featured_articles_category'); 
-  $args = array( 'posts_per_page' => -1, 'cat' => $featured_articles_category);
+  $args = array( 'posts_per_page' => 6, 'cat' => $featured_articles_category);
   $the_query = new WP_Query($args);
   if ($the_query->have_posts()):
   ?>
@@ -12,12 +12,12 @@
           $featured_articles_category_link = get_category_link($featured_articles_category);
           echo '<a href="'.$featured_articles_category_link.'" title="Link to '.$featured_articles_category_name.' Category Page">'.$featured_articles_category_name.'</a>';
           }else{
-            echo 'Featured Articles';
+            echo __('Featured Articles');
           }
         ?>
       </h1>
       <?php  while ( $the_query->have_posts() ) : $the_query->the_post();?>
-      <a title="Link to <?php the_title();?>" href="<?php the_permalink();?>" class="three_span post">
+      <a title="Link to <?php the_title();?>" href="<?php the_permalink();?>" class="four_span post">
         <?php include 'hero_image_variables.php'; ?>
         <span class="thumbnail" style="background-image: url(<?php echo $thumbnail_hero_url; ?>)"></span>
         <span class="category"><?php echo get_the_category()[0]->cat_name;?></span>
