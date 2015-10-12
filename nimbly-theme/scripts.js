dfd86d2996b51a946f36fd86565e55b49781c8c0
Blake Bertuccelli <blakebertuccelli@gmail.com>
2015-10-12 09:16:31 -0500

    Removed Sticky navigation

--- nimbly-theme/scripts.js
+++ nimbly-theme/scripts.js
@@ -5,9 +5,4 @@
     $(this).toggleClass("open");
   });
   
-	//Stick Navigation - sticky-kit.min.js
-	$( "#header, #hero" ).addClass("inside_sticky_area");
-	$( ".inside_sticky_area" ).wrapAll( "<div id='sticky_wrapper' />");
-	$("#header").stick_in_parent();
-
 })( jQuery );
